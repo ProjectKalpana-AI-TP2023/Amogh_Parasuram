97e83df03201e7149b72fa15877fd01a4043f76b
AmoghParasuram <133594267+AmoghParasuram@users.noreply.github.com>
2023-09-17 14:04:14 +0530

    Add files via upload

--- repository Amogh Parauram/task3.cpp
+++ repository Amogh Parauram/task3.cpp
@@ -1,43 +1,57 @@
-#include <ros/ros.h>
+#include <rclcpp/rclcpp.hpp>
+#include <turtlesim/Pose.h>
 #include <geometry_msgs/Twist.h>
 
-int main(int argc, char** argv)
+class SpiralTurtleNode : public rclcpp::Node
 {
-    // Initialize the ROS node
-    ros::init(argc, argv, "turtle_expanding_spiral");
-    ros::NodeHandle nh;
-
-    // Create a publisher to send velocity commands
-    ros::Publisher cmd_vel_pub = nh.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 10);
-
-    // Set the loop rate (adjust this according to your needs)
-    ros::Rate loop_rate(10);
-
-    double linear_speed = 1.0;   // Initial linear speed
-    double angular_speed = 1.0;  // Initial angular speed
-    double linear_increment = 0.1;
-    double angular_increment = 0.1;
-
-    while (ros::ok())
+public:
+    SpiralTurtleNode()
+        : Node("spiral_turtle")
     {
-        // Create a Twist message to send velocity commands
-        geometry_msgs::Twist msg;
+        cmd_pub_ = create_publisher<geometry_msgs::msg::Twist>("/turtle1/cmd_vel", 10);
+        pose_sub_ = create_subscription<turtlesim::msg::Pose>("/turtle1/pose", 10, std::bind(&SpiralTurtleNode::poseCallback, this, std::placeholders::_1));
 
-        // Set the linear and angular velocities
-        msg.linear.x = linear_speed;
-        msg.angular.z = angular_speed;
+        timer_ = create_wall_timer(std::chrono::milliseconds(100), std::bind(&SpiralTurtleNode::update, this));
 
-        // Publish the velocity command
-        cmd_vel_pub.publish(msg);
-
-        // Increase linear and angular speeds for an expanding spiral motion
-        linear_speed += linear_increment;
-        angular_speed += angular_increment;
-
-        // Update the loop
-        ros::spinOnce();
-        loop_rate.sleep();
+        linear_vel_ = 1.0;  // Initial linear velocity
+        angular_vel_ = 0.2; // Initial angular velocity
+        radius_ = 1.0;      // Initial radius
     }
 
+private:
+    void poseCallback(const turtlesim::msg::Pose::SharedPtr msg)
+    {
+        current_pose_ = *msg;
+    }
+
+    void update()
+    {
+        geometry_msgs::msg::Twist cmd_vel;
+        cmd_vel.linear.x = linear_vel_;
+        cmd_vel.angular.z = angular_vel_;
+
+        cmd_pub_->publish(cmd_vel);
+
+        // Increase the linear velocity and expand the spiral
+        linear_vel_ += 0.1;
+        radius_ += 0.1;
+        angular_vel_ = linear_vel_ / radius_;
+    }
+
+    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr cmd_pub_;
+    rclcpp::Subscription<turtlesim::msg::Pose>::SharedPtr pose_sub_;
+    rclcpp::TimerBase::SharedPtr timer_;
+    turtlesim::msg::Pose current_pose_;
+    double linear_vel_;
+    double angular_vel_;
+    double radius_;
+};
+
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<SpiralTurtleNode>());
+    rclcpp::shutdown();
     return 0;
 }
+
