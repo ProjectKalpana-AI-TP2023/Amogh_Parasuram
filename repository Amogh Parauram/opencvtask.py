1c9e74f4c43102c2cdd7e6195a68f4bdd3b4cc46
AmoghParasuram <133594267+AmoghParasuram@users.noreply.github.com>
2023-10-08 23:13:27 +0530

    Add files via upload

---
+++ repository Amogh Parauram/opencvtask.py
@@ -0,0 +1,48 @@
+import cv2
+import numpy as np
+
+def track_green_ball():
+    cap = cv2.VideoCapture(0)
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if not ret:
+            break
+
+        # Convert the frame to HSV color space
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+        # Define the lower and upper bounds for the green color
+        lower_green = np.array([40, 40, 40])
+        upper_green = np.array([80, 255, 255])
+
+        # Threshold the HSV image to get only green colors
+        mask = cv2.inRange(hsv, lower_green, upper_green)
+
+        # Find contours in the mask
+        contours, _ = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        # Draw the contours on the original frame
+        if len(contours) > 0:
+            largest_contour = max(contours, key=cv2.contourArea)
+            (x, y), radius = cv2.minEnclosingCircle(largest_contour)
+            center = (int(x), int(y))
+            radius = int(radius)
+            cv2.circle(frame, center, radius, (0,255 , 0), 2)
+
+        # Display the frame
+        cv2.imshow('Green Ball Tracking', frame)
+
+        # Break the loop if 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    # Release the video capture object and close all windows
+    cap.release()
+    cv2.destroyAllWindows()
+
+# Specify the path to your video file
+
+print("hello")
+# Call the function to track the green ball
+track_green_ball()
